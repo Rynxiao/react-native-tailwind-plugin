95c0770dec4c0eb38e52f6efd4f67449ef256703
GitHub Action <action@github.com>
2021-09-07 02:04:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/rynxiao/reactnativetailwindplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/rynxiao/reactnativetailwindplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rynxiao.reactnativetailwindplugin.listeners
 
+import com.github.rynxiao.reactnativetailwindplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/rynxiao/reactnativetailwindplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rynxiao.reactnativetailwindplugin.services
+
+import com.github.rynxiao.reactnativetailwindplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
